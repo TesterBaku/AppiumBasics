da319115012b398abc3c938e80692100992a44e3
TesterBaku <TesterBaku@yahoo.com>
2020-07-31 23:40:35 -0700

    added apk, base class, basic test

---
+++ src/main/java/basics.java
@@ -0,0 +1,33 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class basics extends base{
+
+	public static void main(String[] args) throws MalformedURLException {
+		
+		AndroidDriver<AndroidElement> driver = Capabilities();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		//best locators: xpath, id, classname, androidUIautomator
+		
+		/** xpath syntax
+		 *  //tagname[@attribute='value']
+		 *  
+		 *  class name can serve as tag name
+		 */
+		driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+		driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
+		driver.findElementById("android:id/checkbox").click();
+		driver.findElementByXPath("(//android.widget.RelativeLayout)[2]").click();
+		
+		
+		
+		
+		
+		
+	}
+
+}
