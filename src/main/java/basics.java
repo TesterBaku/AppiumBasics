35e842f776cb46083f74a28afcf297c2171054bc
TesterBaku <TesterBaku@yahoo.com>
2020-08-01 11:45:02 -0700

    added some more steps to basics file

--- src/main/java/basics.java
+++ src/main/java/basics.java
@@ -15,14 +15,16 @@ public class basics extends base{
 		
 		/** xpath syntax
 		 *  //tagname[@attribute='value']
-		 *  
 		 *  class name can serve as tag name
+		 *  
+		 *  Below are some ways of finding element
 		 */
 		driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
 		driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
 		driver.findElementById("android:id/checkbox").click();
 		driver.findElementByXPath("(//android.widget.RelativeLayout)[2]").click();
-		
+		driver.findElementByClassName("android.widget.EditText").sendKeys("aha");
+		driver.findElementsByClassName("android.widget.Button").get(1).click();
 		
 		
 		
